5370e110c208a95efadd92ee301427d46f1cb56c
jofiel <jag.lurel@gmail.com>
2017-11-28 16:12:39 +0100

    Debut IHM
    Début de code IHM

---
+++ src/jeulangue/Main.java
@@ -0,0 +1,28 @@
+package jeulangue;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("startJeu.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+
+    }
+}
